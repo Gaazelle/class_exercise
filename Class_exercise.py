a5d016cb26dc7f94f503018dd142ad89834f7ae2
Ghazal Asghari <86004688+Gaazelle@users.noreply.github.com>
2022-05-03 19:38:32 -0400

    Add files via upload

---
+++ Class_exercise.py
@@ -0,0 +1,8 @@
+#Class_Excercise_1
+
+num_1 = int(input ('Enter First Number:'))
+num_2 = int(input ('Enter Second Number:'))
+print(num_1+num_2)
+print(num_1*num_2)
+print(num_1**num_2)
+print(num_1%num_2)
